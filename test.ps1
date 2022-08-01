cd162bc4277fece6bdf56d7aed28091ecb471dc7
Catalin <you@example.com>
2022-08-01 20:11:36 +0300

    powershell script

---
+++ test.ps1
@@ -0,0 +1,22 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]$breed,
+    [bool]$list,
+    [bool]$count,
+    [bool]$image
+)
+$dogserver = 'https://dog.ceo/api'
+$cmdOutput = Invoke-RestMethod "$dogserver/breed/$breed/list" | Select-Object -Property 'message' -ExpandProperty message
+if ($list){
+    Write-Host $cmdOutput
+    }
+if ($count){
+    $myList = $cmdOutput.split(' ')
+    $counter = $(echo $myList | measure).Count
+    Write-Host $counter
+    }
+
+
+
